d54c341d3ddcbccfb8c75968b2c65962b898fa34
SimLabJW <20181586@edu.hanbat.ac.kr>
2022-10-06 02:35:28 +0900

    ddddd

--- database/db_manager.py
+++ database/db_manager.py
@@ -1,14 +1,7 @@
 import pymysql
 import sqlite3
-<<<<<<< HEAD
+
 from .config import *
-=======
-<<<<<<< HEAD
-from .config import *
-=======
-from config import *
->>>>>>> 4f3b6b69ecd2e1e4e119872031bf1e7c1ca6b75f
->>>>>>> 9c2db9c4d2e7b0c85d66c1bcd37a119892a830e6
 
 class DBManager:
     """
